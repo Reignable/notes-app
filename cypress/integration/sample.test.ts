3e2e67d66b8d161a598b2941678e279466bbe8cb
Reignable <joebarrett401@gmail.com>
2020-03-21 11:44:56 +0000

    Add deploy to production

--- cypress/integration/sample.test.ts
+++ cypress/integration/sample.test.ts
@@ -1,6 +1,6 @@
 describe('first test', () => {
   it("doesn't do much", () => {
     cy.visit('/')
-    cy.get('[data-testid=app-root]').contains('Hello')
+    cy.get('[data-testid=app-root]').contains('Hi')
   })
 })
