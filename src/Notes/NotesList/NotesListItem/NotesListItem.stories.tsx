a375aef9f382eb9779f52874135edad6a4fcbdb2
Reignable <joebarrett401@gmail.com>
2020-04-04 11:32:20 +0100

    Clean up storybook

--- src/Notes/NotesList/NotesListItem/NotesListItem.stories.tsx
+++ src/Notes/NotesList/NotesListItem/NotesListItem.stories.tsx
@@ -1,3 +1,4 @@
+import { action } from '@storybook/addon-actions'
 import React from 'react'
 import { NotesListItem } from './NotesListItem'
 
@@ -5,5 +6,18 @@ const note = { id: 0, title: 'Test note', body: 'Test note body' }
 
 export default { title: 'Notes/Notes List Item' }
 
-export const NotSelected = () => <NotesListItem {...note} />
-export const Selected = () => <NotesListItem {...note} selected />
+export const NotSelected = () => (
+  <NotesListItem
+    {...note}
+    onDelete={action('onDelete')}
+    onSelect={action('onSelect')}
+  />
+)
+export const Selected = () => (
+  <NotesListItem
+    {...note}
+    selected
+    onDelete={action('onDelete')}
+    onSelect={action('onSelect')}
+  />
+)
