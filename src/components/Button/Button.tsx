9ff05db98b920e832ac71dc604b132a2c1ef4011
Reignable <joebarrett401@gmail.com>
2020-05-24 12:24:23 +0100

    Sort default props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variation?: 'primary' | 'secondary'
+  variation: 'primary' | 'secondary'
+}
+
+const defaultProps: Pick<ButtonProps, 'variation'> = {
+  variation: 'primary',
 }
 
 const StyledButton = styled.button<ButtonProps>`
   background-color: ${props =>
-    props.theme.button[props.variation || 'primary'].backgroundColor};
+    props.theme.button[props.variation].backgroundColor};
   border: none;
   border-radius: 16px;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
   box-sizing: content-box;
-  color: ${props => props.theme.button[props.variation || 'primary'].color};
+  color: ${props => props.theme.button[props.variation].color};
   font-family: 'Source Sans Pro', sans-serif;
+  font-size: 14px;
   font-weight: 600;
   text-transform: uppercase;
   padding: 16px;
@@ -22,25 +27,27 @@ const StyledButton = styled.button<ButtonProps>`
 
   &:hover {
     background-color: ${props =>
-      props.theme.button[props.variation || 'primary'].hover.backgroundColor};
+      props.theme.button[props.variation].hover.backgroundColor};
   }
 
   &:focus {
     box-shadow: 0 0 0 2px
-        ${props =>
-          props.theme.button[props.variation || 'primary'].focus.borderColor},
+        ${props => props.theme.button[props.variation].focus.borderColor},
       0px 4px 6px rgba(0, 0, 0, 0.25);
     outline: none;
   }
 
   &:active {
     box-shadow: 0 0 0 2px
-        ${props =>
-          props.theme.button[props.variation || 'primary'].focus.borderColor},
+        ${props => props.theme.button[props.variation].focus.borderColor},
       0px 2px 2px rgba(0, 0, 0, 0.25);
   }
 `
 
+StyledButton.defaultProps = defaultProps
+
 export const Button = ({ variation = 'primary', ...rest }: ButtonProps) => (
   <StyledButton variation={variation} {...rest} />
 )
+
+Button.defaultProps = defaultProps
